1a5d1348d03bf4a820cbc66bf5e2cc5bf4a01f35
ManasDas-9801 <heyiammanas@gmail.com>
2021-11-09 21:19:41 +0530

    For loop

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home data="This is manas das" />
   </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+      Home,
+      
   }
-}
+};
 </script>
 
 <style>
---
+++ src/components/Home.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+         <h1>Users Details</h1>
+        <table border="2px">
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+          <tr v-for="user in users" :key="user.id">
+                  <td>{{user.id}}</td>
+                  <td>{{user.name}}</td>
+                  <td>{{user.email}}</td>
+          </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props:{
+        data:String
+      },
+      data()
+      {
+        return{
+          users:[
+            {id:1,name:'peter',email:'peter@gmail.com'},
+            {id:2,name:'akon',email:'akon@gmail.com'},
+            {id:3,name:'dkon',email:'dkon@gmail.com'},
+            {id:4,name:'bkon',email:'bkon@gmail.com'},
+          ]
+        }
+      }
+ 
+};
+</script>
